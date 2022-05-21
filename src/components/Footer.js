eb8a4a533ed27ce32b9c771b2bb4406a89693608
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-21 20:11:56 +0700

    Membuat style pada component footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer className="footer">
+        <div class="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a> |
+          Coded by <a style={{ textDecoration: "none", color: "grey" }} href="https://github.com/deezycxde" target="_blank">Yudistira Eka Pratama</a> |
+          Project Repo <a style={{ textDecoration: "none", color: "lightskyblue" }} href="https://github.com/deezycxde/react-rockpaperscissors" target="_blank">To This Github Repo's</a> 
+        </div>
+        <button>Rules</button>
+      </footer>
+    </>
   )
 }
 
