4fda04ae3da02b6c291b58c440287ec0f923ed3d
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-21 09:28:03 +0700

    Menambahkan routing dengan react router dom kepada component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import Header from "./components/Header";
 import Play from "./components/Play";
 import Game from "./components/Game";
 import Footer from "./components/Footer";
-import Modal from "./components/Modal";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <div className="container">
         <Header />
+        <Routes>
+          <Route path="/" element={<Play />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
       </div>
+      <Footer />
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./styles.scss";
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>
-, mountNode);
+const root = ReactDOM.createRoot(document.getElementById("app"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
+
+// var mountNode = document.getElementById("app");
+// ReactDOM.render(
+//   <Router>
+//     <App />
+//   </Router>
+// , mountNode);
