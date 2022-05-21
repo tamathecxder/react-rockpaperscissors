6228e5a6ce63f0cfa98b8dd03569040a08fea769
deezycxde <yudistiraeka.pratama012@gmail.com>
2022-05-21 20:37:22 +0700

    Menstruktur ulang modal dan sekaligus passing props dari parent ke child component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,7 +16,7 @@ const Footer = () => {
           Coded by <a style={{ textDecoration: "none", color: "grey" }} href="https://github.com/deezycxde" target="_blank">Yudistira Eka Pratama</a> |
           Project Repo <a style={{ textDecoration: "none", color: "lightskyblue" }} href="https://github.com/deezycxde/react-rockpaperscissors" target="_blank">To This Github Repo's</a> 
         </div>
-        <button className='rules'>Rules</button>
+        <button className='rules' onClick={toggle}>Rules</button>
       </footer>
       {
         modal ? <Modal toggle={toggle} /> : null
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
-const Modal = () => {
-  return (
-    <div>Modal</div>
-  )
+const Modal = ({ toggle }) => {
+  return ReactDOM.createPortal(
+    <div className="modal-container">
+      <div className="modal-box">
+        <div className="modal__header">
+          <h1>Rules</h1>
+          <button onClick={toggle}>
+            <img src="../images/icon-close.svg" alt="Close" />
+          </button>
+        </div>
+        <img src="../images/image-rules.svg" alt="ImageRules" />
+      </div>
+    </div>, document.getElementById('modal')
+  );
 }
 
 export default Modal
